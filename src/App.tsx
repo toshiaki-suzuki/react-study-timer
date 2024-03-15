cc1d9d613a55ee306c5698fb96fd696a7e679c50
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-15 10:05:48 +0900

    make simple dash board

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import studyImage from './cat_study.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={studyImage} alt="cat_study" />
+        <p>学習時間を記録しよう</p>
+        <div className='Button-Area'>
+          <button type="button">はじめる</button>
+          <button type="button" style={{ marginLeft: '10px' }}>ログイン</button>
+        </div>
       </header>
     </div>
   );
