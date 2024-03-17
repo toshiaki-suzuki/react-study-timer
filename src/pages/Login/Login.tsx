988d15564f62f51056125b11fa8749da6f77017e
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-17 16:58:31 +0900

    Design Login Component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,17 @@
+import './Login.css';
+
 function AppRouter() {
   return (
-    <div>ログイン画面 </div>
+    <div>
+      <h1>ログイン</h1>
+      <label htmlFor="user_id">ユーザーID</label>
+      <input type="text" id="user_id" name="user_id" required/><br/>
+
+      <label htmlFor="password">パスワード</label>
+      <input type="password" id="password" name="password" required /><br/>
+
+      <input type="submit" value="ログイン" />
+    </div>
   );
 }
 
