8caece7f8487d87dbdd6163ccd104b4baf104312
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-17 09:34:55 +0900

    Add Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import studyImage from './cat_study.png';
 import './App.css';
+import Login from './pages/Login/Login';
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={studyImage} alt="cat_study" />
-        <p>学習時間を記録しよう</p>
-        <div className='Button-Area'>
-          <button type="button">はじめる</button>
-          <button type="button" style={{ marginLeft: '10px' }}>ログイン</button>
-        </div>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <header className="App-header">
+              <img src={studyImage} alt="cat_study" />
+              <p>学習時間を記録しよう</p>
+              <div className='Button-Area'>
+                <button type="button">はじめる</button>
+                <button type="button" style={{ marginLeft: '10px' }} onClick={handleLoginClick}>
+                  ログイン
+                </button>
+              </div>
+            </header>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+function AppRouter() {
+  return (
+    <div>ログイン画面 </div>
+  );
+}
+
+export default AppRouter;
