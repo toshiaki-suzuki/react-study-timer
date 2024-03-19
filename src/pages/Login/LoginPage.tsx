4af2690420567154905c7ac8b99bf8b15134f6cb
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-19 15:48:44 +0900

    Add sign in functionality

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,16 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import './LoginPage.css';
 
 function AppRouter() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/auth/sign-in', {
+        email: email,
+        password: password
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>ログイン</h1>
-      <label htmlFor="user_id">ユーザーID</label>
-      <input type="text" id="user_id" name="user_id" required/><br/>
+      <label htmlFor="email">メールアドレス</label>
+      <input type="text" id="email" name="email" required onChange={e => setEmail(e.target.value)} /><br/>
 
       <label htmlFor="password">パスワード</label>
-      <input type="password" id="password" name="password" required /><br/>
+      <input type="password" id="password" name="password" required onChange={e => setPassword(e.target.value)} /><br/>
 
-      <input type="submit" value="ログイン" />
+      <input type="submit" value="ログイン" onClick={handleLogin} />
     </div>
   );
 }
